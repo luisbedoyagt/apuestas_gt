8c7f32f6ab63f122ac18dbce89314d4b59ba5946
luisbedoyagt <90284786+luisbedoyagt@users.noreply.github.com>
2025-08-26 21:49:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,19 @@ function parseNumberString(val) {
 const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyP6dc9ww4I9kw26fQCc0gAyEtYbQVg6DsoAtlnxqhFFJClOrHoudM8PdnBnT9YBopSlA/exec";
 let teamsByLeague = {};
 const leagueNames = { 
-  "WC": "FIFA World Cup", "CL": "UEFA Champions League", "BL1": "Bundesliga", 
-  "DED": "Eredivisie", "BSA": "Campeonato Brasileiro", "PD": "Liga Española", 
-  "FL1": "Ligue 1", "ELC": "Championship", "PPL": "Primeira Liga", 
-  "EC": "European Championship", "SA": "Serie A", "PL": "Premier League" 
+  -- Selecciona liga --
+  "WC": "FIFA World Cup", 
+  "CL": "UEFA Champions League", 
+  "BL1": "Bundesliga", 
+  "DED": "Eredivisie", 
+  "BSA": "Campeonato Brasileiro", 
+  "PD": "Liga Española", 
+  "FL1": "Ligue 1", 
+  "ELC": "Championship", 
+  "PPL": "Primeira Liga", 
+  "EC": "European Championship", 
+  "SA": "Serie A", 
+  "PL": "Premier League" 
 };
 
 function normalizeTeam(raw) {
@@ -376,3 +385,4 @@ function calculateAll() {
     pHome: probs.pHome, pDraw: probs.pDraw, pAway: probs.pAway, pBTTS: probs.pBTTS, pO25: probs.pO25
   });
 }
+

97bef4174fe36e1f4c6f46ce35f80d8fd18f091b
luisbedoyagt <90284786+luisbedoyagt@users.noreply.github.com>
2025-08-29 11:20:36 -0600

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function dixonColesAdjustment(lambdaH, lambdaA, h, a, tau = 0.9) {
 // ----------------------
 // CONFIGURACIÓN DE LIGAS
 // ----------------------
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyKeze4Q381JYVCx4RabKZ61oV9b4rUjt74GHmLbiXvI99yq6FKhQ4mLpoeH-0HPc8oHA/exec"; // Reemplaza con el URL de tu despliegue
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbz8QAZII7FHjPEbsc1LsFZ5nfQlS3P8Fqt5VE3_OAano63Vzle2WeXNBULJoFkaPyrY0w/exec"; // Reemplaza con el URL de tu despliegue
 let teamsByLeague = {};
 
 const leagueNames = {
@@ -564,4 +564,5 @@ function calculateAll() {
   suggestionEl.classList.add('pulse');
   setTimeout(() => suggestionEl.classList.remove('pulse'), 1000);
 }
+
 
